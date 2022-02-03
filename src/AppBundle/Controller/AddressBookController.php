5fb83d459905b3c9d991ba2f0b7a27e27114d5f5
Joshua Ayeni <larryjmpk@gmail.com>
2022-02-03 18:39:38 +0100

    created crud templates and updated controller

--- src/AppBundle/Controller/AddressBookController.php
+++ src/AppBundle/Controller/AddressBookController.php
@@ -13,8 +13,48 @@ class AddressBookController extends Controller
      */
     public function indexAction(Request $request)
     {
-        die('ji');
+        //('ji');
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [ ]);
+        return $this->render('address-book/index.html.twig', [ ]);
+    }
+
+     /**
+     * @Route("/address/create", name="create")
+     */
+    public function createAction(Request $request)
+    {
+        //('ji');
+        // replace this example code with whatever you need
+        return $this->render('address-book/create.html.twig', [ ]);
+    }
+
+     /**
+     * @Route("/address/edit/{id}", name="edit")
+     */
+    public function editAction(Request $request)
+    {
+        //('ji');
+        // replace this example code with whatever you need
+        return $this->render('address-book/edit.html.twig', [ ]);
+    }
+
+     /**
+     * @Route("/address/show/{id}", name="show")
+     */
+    public function showAction(Request $request)
+    {
+        //('ji');
+        // replace this example code with whatever you need
+        return $this->render('address-book/show.html.twig', [ ]);
+    }
+
+     /**
+     * @Route("/address/delete/{id}", name="delete")
+     */
+    public function deleteAction(Request $request)
+    {
+        //('ji');
+        // replace this example code with whatever you need
+        //return $this->render('address-book/index.html.twig', [ ]);
     }
 }
